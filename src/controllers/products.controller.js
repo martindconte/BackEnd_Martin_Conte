f56e8f2b0d54f6b847b071b78d8c86cab29ec246
martindconte <martind.conte85@gmail.com>
2024-02-12 20:45:29 -0300

    Se finalizo la resolucion del endpoint api/products/ method: GET. Se agrego paginacion de mongoose-paginate-v2

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,11 +2,41 @@ import { productsModel } from "../dao/models/products.models.js";
 
 const getProducts = async (req, res) => {
     try {
-        const products = await productsModel.find().limit(req.query.limit)
-        res.send(products)
+        let { limit, page, query, sort } = req.query
+
+        const filter = query ? {
+            $and: [
+                { category: query.toLowerCase() },
+                { stock: { $gt: 0 }
+            },
+            ],
+        } : {}
+
+        const options = {
+            limit: isNaN(parseInt(limit)) ? 10 : parseInt(limit),
+            page: isNaN(parseInt(page)) ? 1 : parseInt(page),
+            sort: { price: sort === 'asc' ? 1 : -1 }
+        }
+
+        const products = await productsModel.paginate(filter, options)
+
+        const paginateData = {
+            status: 'success',
+            payload: products.docs,
+            totalPages: products.totalPages,
+            prevPage: products.prevPage,
+            nextPage: products.nextPage,
+            page: products.page,
+            hasPrevPage: products.hasPrevPage,
+            hasNextPage: products.hasNextPage,
+            prevLink: products.hasPrevPage ? `/api/products?page=${products.prevPage}` : null,
+            nextLink: products.hasNextPage ? `/api/products?page=${products.nextPage}` : null
+        };
+
+        res.send(paginateData)
     } catch (error) {
         console.log(error)
-        res.status(500).send({ error: 'Internal server error' });
+        res.status(500).send({ status: 'error', error: 'Internal server error. The database query could not be performed' });
     }
 }
 
@@ -31,13 +61,13 @@ const addProducts = async (req, res) => {
         res.send(newProduct)
     } catch (error) {
         console.log('Desde controller addproduct', error.message)
-        res.status(500).send( error.message );
+        res.status(500).send(error.message);
     }
 }
 
 const updatedProduct = async (req, res) => {
     try {
-        
+
         const product = await productsModel.findByIdAndUpdate(
             { _id: req.params.pid },
             { ...req.body },
@@ -52,12 +82,14 @@ const updatedProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
     try {
-        const result  = await productsModel.deleteOne({ _id: req.params.pid })
+        const result = await productsModel.deleteOne({ _id: req.params.pid })
         result.deletedCount > 0
-            ? res.status(200).send({ message: `Product id: ${req.params.pid} was successfully deleted`})
-            : res.status(404).send( { error: {
-                message: `Product id: ${req.params.pid} not found!`
-            }} )
+            ? res.status(200).send({ message: `Product id: ${req.params.pid} was successfully deleted` })
+            : res.status(404).send({
+                error: {
+                    message: `Product id: ${req.params.pid} not found!`
+                }
+            })
     } catch (error) {
         console.log(error.message)
         res.status(404).send({ error });
--- src/dao/models/products.models.js
+++ src/dao/models/products.models.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2'
 
 const productsSchema = new mongoose.Schema({
     title: {
@@ -42,4 +43,5 @@ const productsSchema = new mongoose.Schema({
     }
 })
 
+productsSchema.plugin(mongoosePaginate)
 export const productsModel = mongoose.model('Products', productsSchema)
